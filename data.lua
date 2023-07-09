825e3e0c408d90f1abb02e7cff6c00fe57e7d279
David Winn <q@qypea.com>
2023-07-09 15:43:59 -0700

    Initial linked chest module, based heavily on LinkedChestCircuit

---
+++ data.lua
@@ -0,0 +1,29 @@
+local chest = util.table.deepcopy(data.raw["linked-container"]["linked-chest"])
+chest.name = "quasar-chest"
+chest.minable.result = "quasar-chest"
+chest.inventory_size = 120
+
+chest.circuit_wire_connection_point = circuit_connector_definitions["chest"].points
+chest.circuit_connector_sprites = circuit_connector_definitions["chest"].sprites
+chest.circuit_wire_max_distance = default_circuit_wire_max_distance
+
+data:extend({
+  chest,
+  {
+    type = "item",
+    name = "quasar-chest",
+    icon = "__base__/graphics/icons/linked-chest-icon.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "storage",
+    order = "a[items]-a[quasar-chest]",
+    place_result = "quasar-chest",
+    stack_size = 10
+  },
+  {
+    type = "recipe",
+    name = "quasar-chest",
+    enabled = true,
+    ingredients = {},
+    result = "quasar-chest"
+  }
+})
