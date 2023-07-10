516bf935f1ad7c4abb895348120e81d266373e2a
David Winn <q@qypea.com>
2023-07-10 06:42:22 -0700

    Teleport items into inventory, out of trash slots periodically

--- control.lua
+++ control.lua
@@ -3,11 +3,7 @@ function setup_inventory()
     if global.inventory_setup == true then
         return
     end
-
-    -- Get access to the global shared inventory
-    local player = game.get_player(1)
-    local force = player.force
-    local inventory = force.get_linked_inventory("quasar-chest", 0)
+    local inventory = game.get_player(1).force.get_linked_inventory("quasar-chest", 0)
 
     -- For each not hidden item prototype
     local index = 1
@@ -44,13 +40,15 @@ end
 script.on_configuration_changed(configuration_changed)
 
 function player_created(event)
-    -- Enable logistics requests panel in inventory, disable logi robots
-    -- messing with it.
+    -- Enable logistics requests panel in inventory, trash slots.
+    -- Note that this enables logi bots to try to help with inventory, but
+    -- they won't usually be able to. Probably best to not create logi bots.
     local player = game.players[event.player_index]
 
     player.force.character_logistic_requests = true
+    player.force.character_trash_slot_count = 10
     if player.character then
-        player.character_personal_logistic_requests_enabled = false
+        player.character_personal_logistic_requests_enabled = true
     end
 
     -- Set up inventory if we get here and it isn't ready
@@ -58,3 +56,74 @@ function player_created(event)
 end
 script.on_event(defines.events.on_player_created, player_created)
 script.on_event(defines.events.on_player_joined_game, player_created)
+
+function try_move(in_inv, out_inv, item, count)
+    -- Limit to what is available to move
+    local available = in_inv.get_item_count(item)
+    if available < count then
+        count = available
+    end
+
+    if count == 0 then
+        return false
+    end
+
+    -- Try to add first, as that can fail if inv is full or whatnot
+    local added = out_inv.insert({name=item, count=count})
+    if added == 0 then
+        return false
+    end
+
+    -- Now remove only what we successfully added
+    local removed = in_inv.remove({name=item, count=added})
+
+    return added ~= 0
+end
+
+function logistics_tick()
+    -- Update the player's inventory based on logistics requests
+    local player = game.get_player(1)
+    local player_inv = player.get_main_inventory()
+    local quasar_inv = player.force.get_linked_inventory("quasar-chest", 0)
+
+    for i=1,1000 do -- for each logistics slot index(Hard limited by factorio to 1000)
+        local request = player.get_personal_logistic_slot(i)
+        if request == nil or request.name == nil then
+            goto continue
+        end
+
+        local player_count = player_inv.get_item_count(request.name)
+
+        if player.cursor_stack ~= nil and player.cursor_stack.valid_for_read and player.cursor_stack.name == request.name then
+            -- Skip trying to fill things into inventory when we have that
+            -- thing in hand. Avoids constantly overfilling items we're
+            -- currently placing.
+            goto continue
+        end
+
+        if request.min ~= nil and player_count < request.min then
+            try_move(quasar_inv, player_inv, request.name, request.min - player_count)
+            -- Doesn't matter if we succeed or not. Try again next tick
+        end
+
+        -- request.max always is max-uint, a really big number We'll have to
+        -- deal with that using the trash inventory instead
+
+        ::continue::
+    end
+
+    local player_trash = player.get_inventory(defines.inventory.character_trash)
+    for item_name, count in pairs(player_trash.get_contents()) do
+        -- Try to move from trash to quasar network
+        local moved_some = try_move(player_trash, quasar_inv, item_name, count)
+
+        -- If unable to move anything, throw it all away
+        -- Allows us to feed things slowly into quasar chest if they're being
+        -- consumed.
+        if moved_some == false then
+            player_trash.remove({name=item_name, count=count})
+        end
+    end
+end
+
+script.on_nth_tick(120, logistics_tick)
