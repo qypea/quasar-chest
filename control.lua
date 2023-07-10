ebd35078f0eecf6eca830cd90456eaed9b7c7593
David Winn <q@qypea.com>
2023-07-09 21:12:17 -0700

    Implement chest sizing, filtering
    Allocate one stack of storage per item type so we never store too many
    items in quasar space.

---
+++ control.lua
@@ -0,0 +1,27 @@
+function configuration_changed()
+    -- Update our inventory filter for new items that mods may have provided
+
+    -- Get access to the global shared inventory
+    local player = game.get_player(1)
+    local force = player.force
+    local inventory = force.get_linked_inventory("quasar-chest", 0)
+
+    -- For each not hidden item prototype
+    local index = 1
+    for item_name, item_value in pairs(game.item_prototypes) do
+        if item_value.flags and item_value.flags["hidden"] == true then
+            goto continue
+        end
+
+        -- Set one slot to filter for it
+        inventory.set_filter(index, item_name)
+        index = index + 1
+
+        ::continue::
+    end
+
+    -- Set bar so no unfiltered slots can be filled with junk
+    inventory.set_bar(index)
+end
+
+script.on_configuration_changed(configuration_changed)
--- data.lua
+++ data.lua
@@ -1,7 +1,9 @@
 local chest = util.table.deepcopy(data.raw["linked-container"]["linked-chest"])
 chest.name = "quasar-chest"
 chest.minable.result = "quasar-chest"
-chest.inventory_size = 120
+chest.inventory_size = 10000 -- Trimmed in creation callback by setting bar
+chest.inventory_type = "with_filters_and_bar"
+chest.gui_mode = "none"
 
 chest.circuit_wire_connection_point = circuit_connector_definitions["chest"].points
 chest.circuit_connector_sprites = circuit_connector_definitions["chest"].sprites
