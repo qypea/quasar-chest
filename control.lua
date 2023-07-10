a8e9c9975f10093b95122ac78dfcf07271c8ea29
David Winn <q@qypea.com>
2023-07-09 21:28:32 -0700

    Set up player logistics options on player creation
    While at it, set up the linked inventory if needed for starting a new
    save.

--- control.lua
+++ control.lua
@@ -1,5 +1,8 @@
-function configuration_changed()
+function setup_inventory()
     -- Update our inventory filter for new items that mods may have provided
+    if global.inventory_setup == true then
+        return
+    end
 
     -- Get access to the global shared inventory
     local player = game.get_player(1)
@@ -27,6 +30,31 @@ function configuration_changed()
     -- that you don't want to store things in this inventory when changing
     -- mods or mod settings.
     inventory.clear()
+
+    global.inventory_setup = true
+end
+
+
+function configuration_changed()
+    -- Force an upgrade of inventory settings
+    global.inventory_setup = false
+    setup_inventory()
 end
 
 script.on_configuration_changed(configuration_changed)
+
+function player_created(event)
+    -- Enable logistics requests panel in inventory, disable logi robots
+    -- messing with it.
+    local player = game.players[event.player_index]
+
+    player.force.character_logistic_requests = true
+    if player.character then
+        player.character_personal_logistic_requests_enabled = false
+    end
+
+    -- Set up inventory if we get here and it isn't ready
+    setup_inventory()
+end
+script.on_event(defines.events.on_player_created, player_created)
+script.on_event(defines.events.on_player_joined_game, player_created)
